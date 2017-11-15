e800ef59d10505287b94e9cd8994f7c182cb02b4
Akshat Jain <jain.akshat546@gmail.com>
2017-11-15 22:59:05 +0530

    Added colors and contactUs

---
+++ README.md
@@ -0,0 +1,1 @@
+# KairoGuard
---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+
+    //Changing the active item in the main menu and sidebar
+    //var rel = $('body[rel]').attr('rel');
+    //$('.main.menu a:eq(' + rel + ') , #side a:eq(' + rel + ')').addClass('active');
+
+    $('.ui.accordion')
+       .accordion()
+    ;
+
+    // create sidebar and attach to menu open
+
+    $('.sideIcon .sidebar.icon').on('click', function() {
+        $('.ui.sidebar').sidebar('toggle');
+
+    });
+    $('#SideMenu .item').on('click',function() {
+              $('.ui.sidebar').sidebar('hide');
+    })
+  //nav.js plugin code
+  //  var nav = responsiveNav(".nav-collapse", {
+  //      animate: true,
+  //      transition: 100000000,
+  //      });
+  //  nav.toggle();
+
+  $("a.menuitem").click(function(){
+   $("a.menuitem.active").removeClass("active");
+   $(this).addClass("active");
+});
+});
+
+
